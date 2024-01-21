7f6e32df8dd0ba15e4f1c78d299a2f877d362bdd
ehandresen <eirik.horgen.andresen@gmail.com>
2024-01-21 19:36:45 +0100

    added Context and Database

--- backend/Contexts/MyContext.cs
+++ backend/Contexts/MyContext.cs
@@ -1,4 +1,16 @@
+using backend.Models;
+using Microsoft.EntityFrameworkCore;
+
 namespace backend.Contexts;
 
+public class MyContext : DbContext
+{
+    public MyContext(DbContextOptions<MyContext> options) : base(options)
+    {
 
+    }
+
+    public DbSet<Driver> Drivers { get; set; }
+
+}
 
--- backend/Program.cs
+++ backend/Program.cs
@@ -1,6 +1,12 @@
+using backend.Contexts;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<MyContext>(
+    options => options.UseSqlite("Data Source=Databases/F1Db.db")
+);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -8,6 +14,8 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+app.UseStaticFiles(); // making wwwRoot folder accessible (html, css, js, images)
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
